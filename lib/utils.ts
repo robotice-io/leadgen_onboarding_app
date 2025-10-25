24794bd1912025780cf402a91555c50b304c1d44
robotice-io <jose@robotice.io>
2025-10-25 14:58:10 +0000

    committed

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-// Minimal className merge utility compatible with shadcn/ui pattern
-// If you prefer full-featured merging, install:
-//   npm i clsx tailwind-merge
-// and replace this with: export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
-
-export function cn(...inputs: Array<string | false | null | undefined>) {
-	return inputs.filter(Boolean).join(" ");
-}
